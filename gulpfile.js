a792920adb1d750ee13d2c624911100925784cac
TomaszGa <tomasz.galina@gmail.com>
2017-05-29 22:58:09 +0200

    build subpages and add content

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@
     const cleanCSS = require('gulp-clean-css');
     const htmlmin = require('gulp-htmlmin');
 
-    gulp.task("sass", function(){
+    gulp.task('sass', function(){
         return gulp.src("app/scss/**/*.scss")
         .pipe(sass())
         .pipe(gulp.dest("app/css"))
@@ -15,13 +15,13 @@
         }))
     });
 
-    gulp.task("watch", ["browserSync", "sass"], function(){
+    gulp.task('watch', ["browserSync", "sass"], function(){
         gulp.watch("app/scss/**/*.scss", ["sass"]);
         gulp.watch('app/*.html', browserSync.reload); 
         gulp.watch('app/js/**/*.js', browserSync.reload); 
     });
 
-    gulp.task("browserSync", function(){
+    gulp.task('browserSync', function(){
         browserSync.init({
             server: {
                 baseDir: "app"
@@ -35,33 +35,19 @@
         .pipe(gulp.dest('dist/img'))
     );
 
-    gulp.task('autoprefixer', () =>
-    gulp.src('app/css/*')
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('dist/css'))
-    );
-
-    gulp.task('minify-css', function() {
-      return gulp.src('app/css/*.css')
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest('dist/css'));
-    });
-
     gulp.task('minify-html', function() {
         return gulp.src('app/**/*.html')
         .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('dist'));
 });
 
-    gulp.task('ship-css', () =>
-    gulp.src('app/css/*')
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest('dist/css'))
+    gulp.task('build-all',['imagemin', 'minify-html'], function(){
+        gulp.src('app/css/*')
+            .pipe(autoprefixer({
+                browsers: ['last 2 versions'],
+                cascade: false
+            }))
+            .pipe(cleanCSS({compatibility: 'ie8'}))
+            .pipe(gulp.dest('dist/css'))
+        }
     );
